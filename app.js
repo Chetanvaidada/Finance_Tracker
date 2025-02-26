f0b01c57b5fb94a0155fd9372ba3595f394f6555
Chetan Vaidada <savageskullpubg@gmail.com>
2025-02-26 23:06:40 +0530

    changes

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 app.use(cookieParser());
+const path = require("path");
+app.set("views", path.join(__dirname, "views")); 
 app.set("view engine","ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));

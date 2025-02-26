64b3a2fef35d8aa1c7c32545a799b45a5891cd76
Chetan Vaidada <savageskullpubg@gmail.com>
2025-02-26 23:11:12 +0530

    changes

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine","ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, "public")));
 
 require("./models/userModel").createUserTable(); 
 require("./models/expenseModel").createExpensesTable(); 

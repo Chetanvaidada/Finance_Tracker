a1c7e215a66942d19be33b042602df2b30f46745
Chetan Vaidada <savageskullpubg@gmail.com>
2025-02-27 01:08:54 +0530

    changes final

--- routes/user.js
+++ routes/user.js
@@ -45,7 +45,7 @@ router.post("/login", async (req, res) => {
 
     try {
         const user = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
-        if (user.rows.length === 0) return res.render("login", { message: "User not found! Please register." });
+        if (user.rows.length === 0) return res.render("login", { message: "Invalid email or password!" });
 
         const validPassword = await bcrypt.compare(password, user.rows[0].password);
         if (!validPassword) return res.render("login", { message: "Invalid email or password!" });

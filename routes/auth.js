fa2e590bdd188afebb95b3e66957e8e3734d167d
Chetan Vaidada <savageskullpubg@gmail.com>
2025-02-26 22:20:24 +0530

    troubleshooting

--- routes/auth.js
+++ routes/auth.js
@@ -6,13 +6,16 @@ const pool = require("../config/db");
 const jwt = require('jsonwebtoken'); 
 const {getBalance,getIncome,getExpense} = require("../controllers/getBalance");
 const { getTransactions } = require("../controllers/transactionController");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
 
 passport.use(
     new GoogleStrategy(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "http://localhost:3000/auth/google/callback",
+            callbackURL: `http://localhost:${PORT}/auth/google/callback`,
         },
         async (accessToken, refreshToken, profile, done) => {
             try {
@@ -56,7 +59,7 @@ router.get(
    async (req, res) => {
         res.cookie("token", req.user.token, {
             httpOnly: true,
-            secure: false, // Set true in production with HTTPS
+            secure: true, // Set true in production with HTTPS
             maxAge: 24 * 60 * 60 * 1000, // 1 day
         });
 
